9820f2a3035e21a3a7a2d2453ba9a6a27fa09b14
jonathanmstr <jonathanaguirreglez@gmail.com>
2016-08-28 00:58:37 +0200

    Primeras referencias

---
+++ Javascript/examples.js
@@ -0,0 +1,11 @@
+//Javascript Constructor
+function person(first,last,age,eye){
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyeColor = eye;
+}
+
+var myFather =  new person("John","Doe",50,"blue");
+var myMother = new person("Sally","Rally",48,"green");
+console.log(myFather);
